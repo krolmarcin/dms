335d551208bcb637bd84163b61eb2e12416029f7
Marcin <szeryfmk@gmail.com>
2017-03-17 13:05:13 +0100

    roles

--- src/main/java/pl/com/bottega/dms/application/impl/StandardDocumentFlowProcess.java
+++ src/main/java/pl/com/bottega/dms/application/impl/StandardDocumentFlowProcess.java
@@ -16,7 +16,6 @@ import pl.com.bottega.dms.model.numbers.NumberGenerator;
 import pl.com.bottega.dms.model.printing.PrintCostCalculator;
 
 @Transactional
-@RequiresAuth
 public class StandardDocumentFlowProcess implements DocumentFlowProcess {
 
     private NumberGenerator numberGenerator;
@@ -36,6 +35,7 @@ public class StandardDocumentFlowProcess implements DocumentFlowProcess {
     }
 
     @Override
+    @RequiresAuth(role = {"QUALITY_STAFF"})
     public DocumentNumber create(CreateDocumentCommand cmd) {
         Document document = new Document(cmd, numberGenerator);
         documentRepository.put(document);
@@ -43,6 +43,7 @@ public class StandardDocumentFlowProcess implements DocumentFlowProcess {
     }
 
     @Override
+    @RequiresAuth(role = {"QUALITY_STAFF"})
     public void change(ChangeDocumentCommand cmd) {
         DocumentNumber documentNumber = new DocumentNumber(cmd.getNumber());
         Document document = documentRepository.get(documentNumber);
@@ -50,12 +51,14 @@ public class StandardDocumentFlowProcess implements DocumentFlowProcess {
     }
 
     @Override
+    @RequiresAuth(role = {"QUALITY_STAFF"})
     public void verify(DocumentNumber documentNumber) {
         Document document = documentRepository.get(documentNumber);
         document.verify(currentUser.getEmployeeId());
     }
 
     @Override
+    @RequiresAuth(role = {"QUALITY_MANAGER"})
     public void publish(PublishDocumentCommand cmd) {
         DocumentNumber documentNumber = new DocumentNumber(cmd.getDocumentNumber());
         Document document = documentRepository.get(documentNumber);
@@ -64,6 +67,7 @@ public class StandardDocumentFlowProcess implements DocumentFlowProcess {
     }
 
     @Override
+    @RequiresAuth(role = {"QUALITY_MANAGER"})
     public void archive(DocumentNumber documentNumber) {
         Document document = documentRepository.get(documentNumber);
         document.archive(currentUser.getEmployeeId());
