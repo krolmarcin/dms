68143ae6894c3421e577dbd2c1812b5759c78bcb
Marcin <szeryfmk@gmail.com>
2017-03-18 11:28:04 +0100

    asyncExecutor added

--- src/main/java/pl/com/bottega/dms/application/DocumentPublishedEmailNotifier.java
+++ src/main/java/pl/com/bottega/dms/application/DocumentPublishedEmailNotifier.java
@@ -2,6 +2,7 @@ package pl.com.bottega.dms.application;
 
 import org.apache.log4j.Logger;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionalEventListener;
 import pl.com.bottega.dms.model.events.DocumentPublishedEvent;
@@ -10,7 +11,13 @@ import pl.com.bottega.dms.model.events.DocumentPublishedEvent;
 public class DocumentPublishedEmailNotifier {
 
     @TransactionalEventListener
+    @Async
     public void documentPublished(DocumentPublishedEvent documentPublishedEvent) {
+        try {
+            Thread.sleep(3000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         Logger.getLogger(DocumentPublishedEvent.class).info("Mailing to recipents!");
     }
 
--- src/main/java/pl/com/bottega/dms/application/PrintDocumentScheduler.java
+++ src/main/java/pl/com/bottega/dms/application/PrintDocumentScheduler.java
@@ -2,6 +2,7 @@ package pl.com.bottega.dms.application;
 
 import org.apache.log4j.Logger;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import pl.com.bottega.dms.model.events.DocumentPublishedEvent;
 
@@ -9,6 +10,7 @@ import pl.com.bottega.dms.model.events.DocumentPublishedEvent;
 public class PrintDocumentScheduler {
 
     @EventListener
+    @Async
     public void documentPublished(DocumentPublishedEvent event) {
         Logger.getLogger(PrintDocumentScheduler.class).info("Scheduling document printing!");
     }
